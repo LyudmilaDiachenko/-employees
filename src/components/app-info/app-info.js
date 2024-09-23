067d3007e1c913f176995522fd9111097b21ceb3
LyudmilaDiachenko <lyudmila.diachenko@gmail.com>
2024-09-23 14:20:32 +0300

    added section info

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,16 @@
+
+import "./app-info.css";
+
+const AppInfo = () => {
+    return (
+        <div className="app-info">
+            <h1>
+                Облік працівників в компанії
+            </h1>
+            <h2>Загальна кількість працівників</h2>
+            <h2>Премію отримають: </h2>
+        </div>
+    )
+}
+
+export default AppInfo;
