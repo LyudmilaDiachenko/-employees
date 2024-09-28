80450bb9bda082f0106c769531d71fe7e1ab25e0
LyudmilaDiachenko <lyudmila.diachenko@gmail.com>
2024-09-28 15:41:19 +0300

    added data

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,16 +7,19 @@ import EmployeesList from "../employees-list/employees-list";
 import EmployeesAddForm from "../employees-add-form/employees-add-form";
 
 function App(){
+    const data = [
+        {name: "Ольга Д.", salary: 400, increase: false, id: 1, star: false},
+        {name: "Дмитро Н.", salary: 1500, increase: false, id: 2, star: false},
+        {name: "Катерина С.", salary: 2300, increase: true, id: 3, star: false}
+    ]
     return(
         <div className="app">
             <AppInfo />
-
             <div className="search-panel">
                 <SearchPanel />
                 <AppFilter />
             </div>
-
-            <EmployeesList />
+            <EmployeesList data={data} />
             <EmployeesAddForm />
         </div>
     )
